9b076ea8c1cb4746dbf0dc2ee19528840e371845
doron wix <doron251@gmail.com>
2018-04-22 23:45:50 +0300

    add quad and year json API

--- server.js
+++ server.js
@@ -27,17 +27,22 @@ var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/:symbolId', function(req, res) {
+router.get('/:period/:symbolId', function(req, res) {
     rp('https://finance.yahoo.com/quote/' + req.params.symbolId +'/financials?ltr=1')
     .then((htmlString) => {
 		let extractor = HtmlExtractor.load(htmlString, {charset: 'UTF-8'});
 		let test = extractor.$.html();
 		let mtch = test.match('\{"context".*\:\{.*\:.*\}\}');
 		let stringToConvert = mtch[0].toString();
-		let json = JSON.parse(stringToConvert.replace(/[\u0019]/g, ''));   
-		let quatData = json.context.dispatcher.stores.QuoteSummaryStore.incomeStatementHistoryQuarterly;
-		let yearlyData = json.context.dispatcher.stores.QuoteSummaryStore.incomeStatementHistory;
-	res.send({quatData,yearlyData});
+		let json = JSON.parse(stringToConvert.replace(/[\u0019]/g, '')); 
+		let data = {};
+		if (req.params.period === 'q') {
+			 data = json.context.dispatcher.stores.QuoteSummaryStore.incomeStatementHistoryQuarterly;
+		}
+		else{
+			 data = json.context.dispatcher.stores.QuoteSummaryStore.incomeStatementHistory;
+		}
+	res.send({data});
     })
     .catch((err) => {
        console.error(err);
