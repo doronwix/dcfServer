4f24e63d6d53916d13f77c54963ab91d71cdf245
doron wix <doron251@gmail.com>
2018-08-05 22:43:58 +0300

    refactor routes

--- routes/sec.js
+++ routes/sec.js
@@ -4,10 +4,19 @@ const rp = require('request-promise');
 const htmlExtractor = require('html-extract-js');
 const parseXbrl = require('parse-xbrl-10k');
 
-router.get('/:type/:symbolId', function(req, res) {
+router.get('/:maxYear/:symbolId', function(req, res) {
 
-	let type = req.params.type,
-    symbolId = req.params.symbolId;
+	let now = new Date();
+	let symbolId = req.params.symbolId,
+	max_year = req.params.maxYear;
+	max_year = parseInt(max_year) < now.getFullYear() ? max_year : now.getFullYear();
+
+	if (!max_year){
+		res.send("please send maximum year to review");
+		return;
+	}
+
+	let min_year = max_year - 10;
 
 	var merged_result = [];
 	
@@ -15,18 +24,25 @@ router.get('/:type/:symbolId', function(req, res) {
 		res.send("please send symbol");
 		return;
 	}
-	let min_year = 2008, max_year = 2018,
-	promise_arr = [], now = new Date(), year;
-	year = now.getFullYear();
+
+
 	
-	while (year > min_year && year <= max_year){
+	promise_arr = [];
+	let currentYear = max_year;
+	
+	while (currentYear >= min_year && currentYear <= max_year){
 
 		promise_arr.push(new Promise(function(resolve,reject){
-			getDocument(year, symbolId, resolve,reject)
+			get_document(currentYear, symbolId, '10-K', resolve,reject)
 		}))
 		
-		year--;
+		currentYear--;
 	}
+	promise_arr.push(new Promise(function(resolve,reject){
+		get_document(max_year, symbolId, '10-Q', resolve,reject);
+	}))
+	
+	
 	Promise.all(promise_arr).then(function(values) {
 		for(let value of values){
 			if ((value) && (Object.keys(value).length !== 0)){
@@ -35,53 +51,49 @@ router.get('/:type/:symbolId', function(req, res) {
 		}		
 		res.send({merged_result})
 	  });
+	function build_url(htmlString, year, type){
+
+		let extractor = htmlExtractor.load(htmlString, {charset: 'UTF-8'});
+		let extractedText = extractor.$(".companyName").text();
+		let regex= /[0]\d+/g
+		let currentCik = parseFloat(regex.exec(extractedText)[0]);
+
+		let extractedAcccess = extractor.$("tr:contains(" + type + "):not(:contains(" + type + "/A))");
+		accessNumberWrapper.set();
+		let accessNumber = accessNumberWrapper.get(year.toString()).exec(extractedAcccess)[0].replace(/-/g,"");
+
+		return 'https://www.sec.gov/Archives/edgar/data/'+ currentCik + '/' + accessNumber;
+	} 
 	
-	function getDocument(current_year, symbolId,  resolve, reject){
-		
-		rp('http://www.sec.gov/cgi-bin/browse-edgar?CIK=' + symbolId + '&Find=Search&owner=exclude&action=getcompany&type=10-k&owner=exclude&count=20')
-			.then((htmlString) => {
-				let year =  parseFloat(current_year.toString().slice(2,4)) + 1;
-				year = year <10 ? "0" + year : year;
+	function get_document(year, symbolId,  type, resolve, reject){
+		let search_params = '&Find=Search&owner=exclude&action=getcompany&type=' + type + '&owner=exclude&count=20';
+		rp('http://www.sec.gov/cgi-bin/browse-edgar?CIK=' + symbolId + search_params)
+		.then((htmlString) => build_url(htmlString, year, type))
+		.then((url) => rp(url))
+		.then((htmlString) => {
 				let extractor = htmlExtractor.load(htmlString, {charset: 'UTF-8'});
-				let extractedText = extractor.$(".companyName").text();
-				let regex= /[0]\d+/g
-				let currentCik = parseFloat(regex.exec(extractedText)[0]);
-				let extractedAcccess = extractor.$("tr:contains('10-K'):not(:contains(10-K/A))");
-				accessNumberWrapper.set();
-				let accessNumber = accessNumberWrapper.get(year.toString()).exec(extractedAcccess)[0].replace(/-/g,"");
-
-				let url = 'https://www.sec.gov/Archives/edgar/data/'+ currentCik + '/' + accessNumber;
+				let html = extractor.$("#main-content").html();
+				let regex = /\/Archives\/edgar\/data\/[0-9]+\/[0-9]+\/[\w]+-[0-9]+\.xml/g;
+				let tenKurl =  	regex.exec(html);
+				if (!tenKurl){
+					console.log("no document in this year:" + year);
+					resolve({});
+				}
+				let url = 'https://www.sec.gov' + tenKurl[0];
 				console.log(url);
-				rp(url).then((htmlString) => {
-						let extractor = htmlExtractor.load(htmlString, {charset: 'UTF-8'});
-						let html = extractor.$("#main-content").html();
-						let regex = /\/Archives\/edgar\/data\/[0-9]+\/[0-9]+\/[\w]+-[0-9]+\.xml/g;
-						let tenKurl =  	regex.exec(html);
-						if (!tenKurl){
-							console.log("no document in this year:" + current_year);
-							resolve({});
-						}
-						let url = 'https://www.sec.gov' + tenKurl[0];
-						console.log(url);
-						rp(url).then((htmlString) => {
-							let parsed_10k = parseXbrl.parseStr(htmlString);
-							if(parsed_10k){
-								resolve(parsed_10k); 
-							}
-				
-					}).catch((err) => {
-						console.error(err + " year:" + current_year);
-						resolve({})
-					});
- 
-
-				})
-  
-	})
-	.catch((err) => {
-		console.error(err + " year:" + current_year);
-		resolve({})
-	 });   	
+				return url;
+			})
+		.then((url) => rp(url))
+		.then((htmlString) => {
+				let parsed_10k = parseXbrl.parseStr(htmlString);
+				if(parsed_10k){
+					resolve(parsed_10k); 
+				}
+		
+		}).catch((err) => {
+			console.error(err + " year:" + year);
+			resolve({})
+		});   	
 	 
 	}
 	
@@ -94,23 +106,23 @@ var accessNumberWrapper = {
 		return this.regExpMap.get(year);
 	},
 	set(){
-		this.regExpMap.set('04',/([0][\d]+)-04-([\d]+)/);
-		this.regExpMap.set('05',/([0][\d]+)-05-([\d]+)/);
-		this.regExpMap.set('06',/([0][\d]+)-06-([\d]+)/);
-		this.regExpMap.set('07',/([0][\d]+)-07-([\d]+)/);
-		this.regExpMap.set('08',/([0][\d]+)-08-([\d]+)/);
-		this.regExpMap.set('09',/([0][\d]+)-09-([\d]+)/);
-		this.regExpMap.set('10',/([0][\d]+)-10-([\d]+)/);
-		this.regExpMap.set('11',/([0][\d]+)-11-([\d]+)/);
-		this.regExpMap.set('12',/([0][\d]+)-12-([\d]+)/);
-		this.regExpMap.set('13',/([0][\d]+)-13-([\d]+)/);
-		this.regExpMap.set('14',/([0][\d]+)-14-([\d]+)/);
-		this.regExpMap.set('15',/([0][\d]+)-15-([\d]+)/);
-		this.regExpMap.set('16',/([0][\d]+)-16-([\d]+)/);
-		this.regExpMap.set('17',/([0][\d]+)-17-([\d]+)/);
-		this.regExpMap.set('18',/([0][\d]+)-18-([\d]+)/);		
-		this.regExpMap.set('19',/([0][\d]+)-19-([\d]+)/);
-		this.regExpMap.set('20',/([0][\d]+)-20-([\d]+)/);
+		this.regExpMap.set('2004',/([0][\d]+)-04-([\d]+)/);
+		this.regExpMap.set('2205',/([0][\d]+)-05-([\d]+)/);
+		this.regExpMap.set('2006',/([0][\d]+)-06-([\d]+)/);
+		this.regExpMap.set('2007',/([0][\d]+)-07-([\d]+)/);
+		this.regExpMap.set('2008',/([0][\d]+)-08-([\d]+)/);
+		this.regExpMap.set('2009',/([0][\d]+)-09-([\d]+)/);
+		this.regExpMap.set('2010',/([0][\d]+)-10-([\d]+)/);
+		this.regExpMap.set('2011',/([0][\d]+)-11-([\d]+)/);
+		this.regExpMap.set('2012',/([0][\d]+)-12-([\d]+)/);
+		this.regExpMap.set('2013',/([0][\d]+)-13-([\d]+)/);
+		this.regExpMap.set('2014',/([0][\d]+)-14-([\d]+)/);
+		this.regExpMap.set('2015',/([0][\d]+)-15-([\d]+)/);
+		this.regExpMap.set('2016',/([0][\d]+)-16-([\d]+)/);
+		this.regExpMap.set('2017',/([0][\d]+)-17-([\d]+)/);
+		this.regExpMap.set('2018',/([0][\d]+)-18-([\d]+)/);		
+		this.regExpMap.set('2019',/([0][\d]+)-19-([\d]+)/);
+		this.regExpMap.set('2020',/([0][\d]+)-20-([\d]+)/);
 	}	
 
 }
